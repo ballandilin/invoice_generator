df3372863f1531dfa0e6dd72d6c72709a023f7e2
ballandilin <nicolas.benoit6@gmail.com>
2022-03-12 16:39:05 +0100

    update html

---
+++ js/main.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+    console.log("onload");
+
+    function genPDF() {
+        const doc = new jspdf.jsPDF({
+            orientation: "landscape",
+            unit: "in",
+            format: [4, 2]
+        });
+
+        doc.text("Hello world!", 1, 1);
+        doc.save("test.pdf");
+
+    }
+
+
+
+
+    let btPDF = document.querySelector("#pdf");
+
+    btPDF.addEventListener("click", () => {
+        console.log("pressed");
+        genPDF();
+    });
+}
